ad389f8bd0998a1753f2ef287d2b0bb160907778
practicante-giro <pacticante1@giro.com.mx>
2020-01-15 10:22:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/prueba/MainK.kt
@@ -0,0 +1,13 @@
+package com.example.prueba
+
+
+fun main(args: Array<String>)
+{
+    fun saludar(vararg nombres:String){
+        for (nombre in nombres){
+            println(nombre)
+        }
+    }
+
+    saludar("carlos","valvenze","suarez")
+}
