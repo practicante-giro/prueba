0a7d2e02d14e13a87d71b1fcee765adcf65e2583
practicante-giro <practicante1@giro.com.mx>
2020-01-15 17:14:16 -0600

    barra oculta

--- app/src/main/java/com/example/prueba/MainActivity.kt
+++ app/src/main/java/com/example/prueba/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 }
